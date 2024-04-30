5de9a237d35afc4bf4c175458535e0f09875127a
DERPZ774 <86765223+DERPZ774@users.noreply.github.com>
2024-04-30 17:07:28 -0400

    Bullet changes

--- src/main/java/com/derpz/nukaisles/entity/custom/BulletProjectileEntity.java
+++ src/main/java/com/derpz/nukaisles/entity/custom/BulletProjectileEntity.java
@@ -3,50 +3,42 @@ package com.derpz.nukaisles.entity.custom;
 import com.derpz.nukaisles.entity.ModEntities;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.projectile.PersistentProjectileEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-public class BulletProjectileEntity extends PersistentProjectileEntity {
+public class BulletProjectileEntity extends ProjectileEntity {
     private float damage;
     private Vec3d vel;
     private int maxLife;
     private int lifeTicks;
 
-    public BulletProjectileEntity(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
-        super(entityType, world);
+    public BulletProjectileEntity(EntityType<BulletProjectileEntity> type, World world) {
+        super(ModEntities.BULLET_PROJECTILE, world);
     }
 
     public BulletProjectileEntity(LivingEntity owner, World world, float bulletDamage) {
-        super(ModEntities.BULLET_PROJECTILE, owner, world);
-        this.maxLife = 10;
-        this.lifeTicks = 0;
+        super(ModEntities.BULLET_PROJECTILE, world);
         this.damage = bulletDamage;
-        this.setNoGravity(true);
+       // this.setNoGravity(true);
+    }
+
+    @Override
+    protected void initDataTracker() {
+
     }
 
     @Override
     public void tick() {
         super.tick();
-        this.setVelocity(vel);
-
-        if(this.lifeTicks++ >= this.maxLife) {
-            this.discard();
-        }
     }
 
-    @Override
-    public boolean hasNoGravity() {
-        return !this.isTouchingWater();
-    }
-
-    @Override
-    protected ItemStack asItemStack() {
-        return null;
-    }
+//    @Override
+//    public boolean hasNoGravity() {
+//        return !this.isTouchingWater();
+//    }
 
     @Override
     protected void onEntityHit(EntityHitResult entityHitResult) {
@@ -54,16 +46,18 @@ public class BulletProjectileEntity extends PersistentProjectileEntity {
         {
             entity.damage(getDamageSources().thrown(this, this.getOwner() != null ? this.getOwner():this), this.damage);
             entity.timeUntilRegen = 0;
+            System.out.println("Hit entity");
         }
         this.discard();
     }
 
     @Override
     protected void onBlockHit(BlockHitResult blockHitResult) {
+        System.out.println("Hit block");
         this.discard();
     }
 
-    public void setVel(Vec3d vel) {
+    public void setBaseVel(Vec3d vel) {
         this.vel = vel;
     }
 }
--- src/main/java/com/derpz/nukaisles/item/custom/TenMmPistolItem.java
+++ src/main/java/com/derpz/nukaisles/item/custom/TenMmPistolItem.java
@@ -1,9 +1,18 @@
 package com.derpz.nukaisles.item.custom;
 
+import com.derpz.nukaisles.entity.custom.BulletProjectileEntity;
 import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.stat.Stats;
 import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,32 +23,18 @@ public class TenMmPistolItem extends Item {
         super(settings);
     }
 
-//    @Override
-//    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-//        ItemStack itemStack = user.getStackInHand(hand); // creates a new ItemStack instance of the user's itemStack in-hand
-//        world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 1F); // plays a globalSoundEvent
-//		/*
-//		user.getItemCooldownManager().set(this, 5);
-//		Optionally, you can add a cooldown to your item's right-click use, similar to Ender Pearls.
-//		*/
-//        if (!world.isClient) {
-//            BulletProjectileEntity bulletProjectileEntity = new BulletProjectileEntity(user, world, 1);
-//            bulletProjectileEntity.getStack();
-//            bulletProjectileEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 0F);
-//                        /*
-//                        snowballEntity.setProperties(user, user.getPitch(), user.getYaw(), 0.0F, 1.5F, 1.0F);
-//                        In 1.17,we will use setProperties instead of setVelocity.
-//                        */
-//            world.spawnEntity(bulletProjectileEntity); // spawns entity
-//        }
-//
-//        user.incrementStat(Stats.USED.getOrCreateStat(this));
-//        if (!user.getAbilities().creativeMode) {
-//            itemStack.decrement(1); // decrements itemStack if user is not in creative mode
-//        }
-//
-//        return TypedActionResult.success(itemStack, world.isClient());
-//    }
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+
+        if (!world.isClient) {
+            BulletProjectileEntity bulletEntity = new BulletProjectileEntity(user, world, 4);
+            bulletEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0f, 1 ,1);
+            world.spawnEntity(bulletEntity);
+        }
+
+        return TypedActionResult.success(itemStack, world.isClient());
+    }
 
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
