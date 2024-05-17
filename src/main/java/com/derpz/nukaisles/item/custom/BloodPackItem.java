2b9fdf57443b119623748e4b5f5b9436e73f348f
Dyna-Minecraft <ryxcory@gmail.com>
2024-05-17 12:31:20 +0100

    fixed a texture bug 🤮🤮🤮 (and added a texture/model for blood pack) (and also fixed no ammo in creative with the guns (i think))

--- src/main/java/com/derpz/nukaisles/item/custom/BloodPackItem.java
+++ src/main/java/com/derpz/nukaisles/item/custom/BloodPackItem.java
@@ -20,39 +20,31 @@ public class BloodPackItem extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient) {
-            startHealing((ServerPlayerEntity) user);
+        if (!world.isClient && user.isAlive()) {
+
             user.getStackInHand(hand).decrement(1);
+//no clue why it doesn't wait...
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+            Main.wait(1000);
+            user.heal(HEAL_AMOUNT / 10);
+
         }
         return super.use(world, user, hand);
-    }
-
-    private void startHealing(ServerPlayerEntity player) {
-        MinecraftServer server = player.getServer();
-
-        server.execute(() -> {
-            if (player.isAlive()) {
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-                Main.wait(1000);
-                player.heal(HEAL_AMOUNT / 10);
-            }
-        });
     }
 }
